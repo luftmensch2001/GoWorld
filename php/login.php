af54b41e452a8770cbd04e39af8c6dbebf7e28ac
tamdoduc <82079157+tamdoduc@users.noreply.github.com>
2022-05-27 14:22:07 +0700

    CheckLogin

--- php/login.php
+++ php/login.php
@@ -1,23 +1,43 @@
+<?php
+require_once('./Controller/Account.php');
+require_once('./Model/AccountDTO.php');
+
+if (isset($_POST['usernameLogin'])) {
+    $username = $_POST['usernameLogin'];
+    $password =  md5($_POST['passwordLogin']);
+
+    $id = AccountDTO::getInstance()->GetId($username, $password);
+    if ($id != -1) {
+        $_SESSION['idAccount'] = $id;
+        include './Home-page.php';
+    } else {
+        echo '<script>alert("Tên tài khoản hoặc mật khẩu không chính xác")</script>';
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tour - Trang chủ</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
-    <link rel="stylesheet" href="./assets/css/base.css">
-    <link rel="stylesheet" href="./assets/css/login.css">
-    <link rel="stylesheet" href="./assets/fonts/fontawesome-free-6.1.1-web/css/all.min.css">
+    <link rel="stylesheet" href="../assets/css/base.css">
+    <link rel="stylesheet" href="../assets/css/login.css">
+    <link rel="stylesheet" href="../assets/fonts/fontawesome-free-6.1.1-web/css/all.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Risque&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
 </head>
+
 <body>
     <header class="header">
         <a class="header__logo" href="#">
-            <img src="./assets/img/Logo.png" alt="" width="60px" height="60px">
+            <img src="../assets/img/Logo.png" alt="" width="60px" height="60px">
             <h2>GoWorld</h2>
         </a>
         <div class="header__navbar">
@@ -30,7 +50,7 @@
                 <div class="header__underline"></div>
             </div>
             <div class="header__navbar-contact">
-                <a href="" >Liên hệ</a>
+                <a href="">Liên hệ</a>
                 <div class="header__underline"></div>
             </div>
         </div>
@@ -39,12 +59,12 @@
             <button class="header__account-btn secondary-btn">Đăng ký</button>
             <!-- <div class="header__account-user">
                 <a href="">
-                    <img src="./assets/img/avatar.png" alt="" class="header__account-user-img">
+                    <img src="../assets/img/avatar.png" alt="" class="header__account-user-img">
                 </a>
                 <div class="header__account-user-menu">
                     <div class="account-user__info">
                         <a href="">
-                            <img src="./assets/img/avatar.png" alt=""  width="30px" height="30px">
+                            <img src="../assets/img/avatar.png" alt=""  width="30px" height="30px">
                             <div>
                                 <span class="account-user__info-name">Họ Và Tên</span> <br>
                                 <span class="account-user__info-sub">Thông tin cá nhân</span> 
@@ -53,15 +73,15 @@
                     </div>
                     <div class="account-user__option">
                         <a href="" class="account-user__option-item">
-                            <img src="./assets/img/history.png" alt="">
+                            <img src="../assets/img/history.png" alt="">
                             <span>Lịch sử đặt tour</span>
                         </a href="">
                         <a href="" class="account-user__option-item">
-                            <img src="./assets/img/setting.png" alt="">
+                            <img src="../assets/img/setting.png" alt="">
                             <span>Cài đặt</span>
                         </a href="">
                         <a href="" class="account-user__option-item">
-                            <img src="./assets/img/logout.png" alt="">
+                            <img src="../assets/img/logout.png" alt="">
                             <span>Đăng xuất</span>
                         </a href="">
                     </div>
@@ -80,32 +100,33 @@
                 </a>
             </div>
             <div class="container__form-text">
-                <span>Chào mừng đến GoWorld! 
-                    Hãy điền tên đăng nhập và mật khẩu để truy 
+                <span>Chào mừng đến GoWorld!
+                    Hãy điền tên đăng nhập và mật khẩu để truy
                     cập bằng tài khoản cá nhân của bạn</span>
             </div>
             <div class="container__form-signup">
                 <form method="POST" action="#" class="form-signup__form">
-                    <input type="text" class="form-signup__form-username" name="usernameSignup" spellcheck="false" placeholder="Tên đăng nhập">
-                    <input type="text" class="form-signup__form-email" name="emailSignup" spellcheck="false" placeholder="Địa chỉ email">
-                    <input type="password" class="form-signup__form-password" name="passwordSignup" spellcheck="false" placeholder="Mật khẩu">
-                    <input type="password" class="form-signup__form-repassword" name="re-passwordSignup" spellcheck="false" placeholder="Nhập lại mật khẩu">
+                    <input type="text" class="form-signup__form-username" name="usernameSignup" spellcheck="false" placeholder="Tên đăng nhập" required>
+                    <input type="text" class="form-signup__form-email" name="emailSignup" spellcheck="false" placeholder="Địa chỉ email" required>
+                    <input type="password" class="form-signup__form-password" name="passwordSignup" spellcheck="false" placeholder="Mật khẩu" required>
+                    <input type="password" class="form-signup__form-repassword" name="re-passwordSignup" spellcheck="false" placeholder="Nhập lại mật khẩu" required>
                 </form>
                 <div class="form-signup__btn-signup">
                     <button>ĐĂNG KÝ</button>
                 </div>
             </div>
             <div class="container__form-login">
-                <form method="post" action="#"class="form-login__form">
-                    <input type="text" class="form-login__form-username" name ="usernameLogin" spellcheck="false" placeholder="Tên đăng nhập">
-                    <input type="password" class="form-login__form-password" name ="passwordLogin" spellcheck="false" placeholder="Mật khẩu">
+                <form method="post" action="#" class="form-login__form">
+                    <input type="text" class="form-login__form-username" name="usernameLogin" spellcheck="false" placeholder="Tên đăng nhập" required>
+                    <input type="password" class="form-login__form-password" name="passwordLogin" spellcheck="false" placeholder="Mật khẩu" required>
+                <div class="form-login__btn-login">
+                    <button>ĐĂNG NHẬP</button>
+                </div>
                 </form>
                 <div>
                     <button class="form-login__forgot-password">Quên mật khẩu?</button>
                 </div>
-                <div class="form-login__btn-login">
-                    <button>ĐĂNG NHẬP</button>
-                </div>
+            </div>
             </div>
         </div>
 
@@ -129,6 +150,7 @@
             <div>Email: gowolrd@gmail.com</div>
         </div>
     </footer>
-    <script src="./assets/js/login.js"></script>
+    <script src="../assets/js/login.js"></script>
 </body>
+
 </html>
